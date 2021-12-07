187769676dbc5c0aafba2a2eead3a076da39cf2d
ahuitric <60372947+ahuitric@users.noreply.github.com>
2021-12-07 14:45:11 -0500

    Working code

--- laptopcam.py
+++ laptopcam.py
@@ -2,6 +2,11 @@ import cv2 # pip install opencv
 #pip install opencv-contrib-python
 import matplotlib.pyplot as plt #pip install numpy
 from deepface import DeepFace #pip install deepface
+#from serial_cmd import Serial_cmd #serial_cmd class is in folder
+import serial
+import time
+
+arduino = serial.Serial(port='COM6', baudrate=115200, timeout=.1)
 
 scale = 25
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -53,12 +58,44 @@ while True:
     color = (255,0,255)
     thickness = 1
     image = cv2.putText(frame, text, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
+    
+    emotion_value = 8
+    if text == 'neutral':
+        emotion_value = 1
+    elif text == 'happy':
+        emotion_value = 2
+    elif text == 'sad':
+        emotion_value = 3
+    elif text == 'angry':
+        emotion_value = 4
+    elif text == 'fear':
+        emotion_value = 5
+    elif text == 'surprise':
+        emotion_value = 6
+    elif text == 'disgust':
+        emotion_value = 7
 
-    #Display in imshow
-    cv2.imshow('Camera', image)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+    
+    arduino_data = arduino.readline()
+    arduino_string = arduino_data.decode("utf-8")
+    
+    if "Seeking" in arduino_string:
+        print("emotion value")
+        print(f'emotion value: {emotion_value}')
+        arduino.write(str(emotion_value).encode("utf-8"))
+        #Display in imshow
+        cv2.imshow('Camera', image)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    else:
+        print("arduino stuff:")
+        arduino_data = arduino.readline()
+        arduino_string = arduino_data.decode("utf-8")
+        print(arduino_string)
+
+
 
 # When everything done, release the capture
+
 cap.release()
 cv2.destroyAllWindows()
--- music_box.ino
+++ music_box.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <Servo.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -7,47 +8,26 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // need to setup connection to serial input from laptop also someone help me
 
 // notes to pins
-Servo C4;
-Servo D4;
-Servo E4;
-Servo F4;
-Servo G4;
-Servo A4;
-Servo As4;
-Servo B4;
-Servo C5;
-Servo Cs5;
-Servo D5;
-Servo Ds5;
-Servo E5;
-Servo F5;// I skipf#5
-Servo G5;
-Servo Gs5;
-Servo A5;
-Servo As5;
-Servo B5;
-Servo C6;
-
-int C4_pos = 0;
-int D4_pos = 0;
-int E4_pos = 0;
-int F4_pos = 0;
-int G4_pos = 0;
-int A4_pos = 0;
-int As4_pos = 0;
-int B4_pos = 0;
-int C5_pos = 0;
-int Cs5_pos = 0;
-int D5_pos = 0;
-int Ds5_pos = 0;
-int E5_pos = 0;
-int F5_pos = 0;
-int G5_pos = 0;
-int Gs5_pos = 0;
-int A5_pos = 0;
-int As5_pos = 0;
-int B5_pos = 0;
-int C6_pos = 0;
+Servo note_C4;
+Servo note_D4;
+Servo note_E4;
+Servo note_F4;
+Servo note_G4;
+Servo note_A4;
+Servo note_As4;
+Servo note_B4;
+Servo note_C5;
+Servo note_Cs5;
+Servo note_D5;
+Servo note_Ds5;
+Servo note_E5;
+Servo note_F5;// I skipf#5
+Servo note_G5;
+Servo note_Gs5;
+Servo note_A5;
+Servo note_As5;
+Servo note_B5;
+Servo note_C6;
 
 int WHOLE = 1000;
 int HALF = 500;
@@ -56,81 +36,165 @@ int EIGHTH = 125;
 
 int state = 1;
 int current_emotion = 0;
-int incomingByte = 0; //for incoming serial data
+String incomingmsg; //for incoming serial data
 int song_status = 0; // change to 1 while playing then 0 when not
 
 void setup() {
-  Serial.begin(9600); // maybe needs to be changed? dont know
+  Serial.begin(115200); // maybe needs to be changed? dont know
   // need to setup connection to serial input from laptop
+  
   AFMS.begin();
-  rightMotor->run(FORWARD);
-  leftMotor->run(FORWARD);
-  C4.attach(0);
-  D4.attach(1);
-  E4.attach(2);
-  F4.attach(3);
-  G4.attach(4);
-  A4.attach(5);
-  As4.attach(6);
-  B4.attach(7);
-  C5.attach(8);
-  Cs5.attach(9);
-  D5.attach(10);
-  Ds5.attach(11);
-  E5.attach(12);
-  F5.attach(13);
-  G5.attach(14);
-  Gs5.attach(15);
-  A5.attach(16);
-  As5.attach(17);
-  B5.attach(18);
-  C6.attach(19);
+  
+  note_C4.attach(0);
+  note_D4.attach(1);
+  note_E4.attach(2);
+  note_F4.attach(3);
+  note_G4.attach(4);
+  note_A4.attach(5);
+  note_As4.attach(6);
+  note_B4.attach(7);
+  note_C5.attach(8);
+  note_Cs5.attach(9);
+  note_D5.attach(10);
+  note_Ds5.attach(11);
+  note_E5.attach(12);
+  note_F5.attach(13);
+  note_G5.attach(14);
+  note_Gs5.attach(15);
+  note_A5.attach(16);
+  note_As5.attach(17);
+  note_B5.attach(18);
+  note_C6.attach(19);
 }
 
-void play_note(note,note_pos, note_length){
-  if (note_pos < 90){ // needs to be changed
-    note.write(90);
-    delay(note_length);
-  }
-  else {
+void play_note(Servo note, int note_length){
+    note.write(30);
+    delay(100);
     note.write(0);
     delay(note_length);
-  }
 }
+
 
 // song methods
 void play_neutral(){ //0
+  Serial.println("Neutral");
+  song_status = 1;
+  play_note(note_Ds5,WHOLE);
+  play_note(note_Ds5,HALF);
+  play_note(note_Ds5,QUARTER);
+  delay(2000);
+  song_status = 0;
 }
 
 void play_happy(){ // 1
+  Serial.println("Happy");
+  song_status = 1;
+  play_note(note_Ds5,HALF);
+  play_note(note_Ds5,HALF);
+  play_note(note_Ds5,HALF);
+  delay(2000);
+  song_status = 0;
 }
 
 void play_sad(){//2
+  Serial.println("Sad");
+  song_status = 1;
+  play_note(note_Ds5,WHOLE);
+  play_note(note_Ds5,WHOLE);
+  play_note(note_Ds5,WHOLE);
+  delay(2000);
+  song_status = 0;
 }
 
 void play_angry(){//3
+  Serial.println("Angry");
+  song_status = 1;
+  play_note(note_Ds5,QUARTER);
+  play_note(note_Ds5,QUARTER);
+  play_note(note_Ds5,QUARTER);
+  delay(2000);
+  // notes go in here
+  song_status = 0;
 }
 
 void play_fear(){//4
+  Serial.println("Fear");
+  song_status = 1;
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,QUARTER);
+  // notes go in here
+  song_status = 0;
 }
 
 void play_surprise(){//5
+  Serial.println("Surprise");
+  song_status = 1;
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,QUARTER);
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,QUARTER);
+  // notes go in here
+  song_status = 0;
 }
 
+void play_disgust(){
+  Serial.println("Disgust");
+  song_status = 1;
+  play_note(note_Ds5,WHOLE);
+  play_note(note_Ds5,EIGHTH);
+  play_note(note_Ds5,QUARTER);
+  // notes go in here
+  song_status = 0;
+}
+
+void play_song(){
+  if (incomingmsg.length() > 0 ){
+    if (incomingmsg.toInt() == 1) {
+      play_neutral();
+    }
+    else if (incomingmsg.toInt() == 2) {
+      play_happy();
+    }
+    else if (incomingmsg.toInt() == 3) {
+      play_sad();
+    }
+    else if (incomingmsg.toInt() == 4) {
+      play_angry();
+    }
+    else if (incomingmsg.toInt() == 5) {
+      play_fear();
+    }
+    else if (incomingmsg.toInt() == 6) {
+      play_surprise();
+    }
+    else if (incomingmsg.toInt() == 7) {
+      play_disgust();
+    }
+  }
+}
 void get_state(){
-  if (state == 1)
+  if (state == 1){
+    song_status = 0;
+    state = 2;
+  } else if (state == 2) {
+    if (song_status == 0);
+      Serial.write("Seeking");
+      state = 1;
+  }
 }
 
 // state machine down here start state, currently playing state, find new song state, end state?
 void loop(){
+      get_state();
   switch(state){
     case 1:
     // not currently playing
-    // read serial input from laptop
-    // change current emotion to mach the output
-    
+      delay(1000);
     case 2:
-    case 3:
+      incomingmsg = Serial.readString();
+      play_song();
   }
-}
 }
